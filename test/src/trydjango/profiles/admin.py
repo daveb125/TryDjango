450c4d3f2ae679e344ef6b86e3deae8bee5f6567
Jeremy <jeremy.singh@gmail.com>
2019-09-02 18:22:09 +0200

    Added Profiles API

--- test/src/trydjango/profiles/admin.py
+++ test/src/trydjango/profiles/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
 # Register your models here.
+# import models here JDS
+from .models import Profile
+admin.site.register(Profile)
--- test/src/trydjango/profiles/models.py
+++ test/src/trydjango/profiles/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+class Profile(models.Model):
+    name = models.TextField()
+    surname = models.TextField()
+    email = models.TextField()
+    password = models.TextField()
+    active = models.TextField()
